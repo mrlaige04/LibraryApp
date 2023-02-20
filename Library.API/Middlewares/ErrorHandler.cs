3caba0b520bcda8b7692766947b3ae3299c01bb3
mrlaige04 <i.rudiakov.ik11@kpi.ua>
2023-02-20 14:12:49 +0200

    fix

--- Library.API/Middlewares/ErrorHandler.cs
+++ Library.API/Middlewares/ErrorHandler.cs
@@ -1,6 +1,53 @@
-﻿namespace Library.API.Middlewares
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Library.API.Middlewares
 {
     public class ErrorHandler
     {
+        private readonly RequestDelegate next;
+
+        public ErrorHandler(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var errorResponse = new ErrorResponse
+                {
+                    Message = "An error occurred while processing your request",
+                    Details = ex.Message
+                };
+
+                var json = JsonSerializer.Serialize(errorResponse);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+
+
+        public class ErrorResponse
+        {
+            public string Message { get; set; }
+            public string Details { get; set; }
+        }
+    }
+
+    public static class ErrorHandlerExtensions
+    {
+        public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandler>();
+        }
     }
 }
